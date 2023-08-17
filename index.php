34a5b41a64c672b8f02fb20d5961d62945dc47e7
Restu Andryana Suhendar <89282495+rest24@users.noreply.github.com>
2023-08-17 14:36:41 +0700

    Update and rename result.php to index.php

--- index.php
+++ index.php
@@ -12,13 +12,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $hari_lahir_name = $hariNames[date("w", $tanggal_lahir)];
         
         echo "<div id='result'>";
-        echo "Umur Silaing: " . $umur_tahun . " tahun<br>";
-        echo "Tanggal : " . date("d F Y", $tanggal_lahir) . "<br>";
-        echo "Lahir Poe : " . $hari_lahir_name . "<br>";
-        echo "Catatan : Geura kawin geus kolot :D";
+        echo "Umur Anda: " . $umur_tahun . " tahun<br>";
+        echo "Tanggal Lahir: " . date("d F Y", $tanggal_lahir) . "<br>";
+        echo "Hari Lahir: " . $hari_lahir_name . "<br>";
         echo "</div>";
     } else {
-        echo "<div id='result'>Pilih heula ...!!</div>";
+        echo "<div id='result'>Pilih tanggal lahir Anda.</div>";
     }
 }
 ?>
